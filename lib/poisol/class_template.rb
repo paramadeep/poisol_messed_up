242c699be21d2de02c09a4d3f0b35e49b98b761b
deepak <anandhadeepak@gmail.com>
2014-09-14 00:30:43 +0530

    made request dynamic for one level

--- lib/poisol/class_template.rb
+++ lib/poisol/class_template.rb
@@ -7,18 +7,31 @@ module ClassTemplate
     config = Parse.yaml_file @base_file
     @url = config.request.url
     @request = Parse.json_file_to_hash (ConfigMap.request self.class.name)
+    generate_request_methods
   end
 
   def build
     make_stub
   end
 
+  def generate_request_methods 
+    @request.each do |field|
+      define_singleton_method("by#{field[0].capitalize}") do |*value|
+        @request[field[0]] = value[0]
+        self
+      end
+    end
+  end
+
   private
   def make_stub 
-    stub_request(:post, "http://http//localhost:7098:80/#{@url}").
-      with(:body => @request,
-           :headers => {'Accept'=>'*/*; q=0.5, application/xml', 'Accept-Encoding'=>'gzip, deflate', 'Content-Length'=>'11', 'Content-Type'=>'application/x-www-form-urlencoded', 'User-Agent'=>'Ruby'}).
-      to_return(:status => 200, :body => "", :headers => {})
+    stub_request(:post, "http://http//localhost:7098:80/users").
+               with(:body => @request).
+                        to_return(:status => 200, :body => "", :headers => {})
+
+    #stub_request(:post, "http://http//localhost:7098:80/#{@url}").
+    #with(:body => @request).
+      #to_return(:status => 200, :body => "", :headers => {})
   end
 
 end
--- spec/factory_spec.rb
+++ spec/factory_spec.rb
@@ -5,11 +5,8 @@ require 'pry'
 describe Factory, "#score" do
   it "returns class for the yaml" do
     factory = Factory.build("data")
-    User.new.build()
-    #User.new.withName('dummmy').build()
-    #expect(user).to eq(42)
-    #Net::HTTP.post_form("http://localhost:7098/users","name"=>"deepak") 
-    RestClient.post "http://http//localhost:7098:80/users","name"=>"deepak"
+    User.new.byName('dummy').build()
+    RestClient.post "http://http//localhost:7098:80/users","name"=>"dummy"
   end
 end
 
