8bfd1811aff7c6f1949ab9a8e7b1db5549fdea39
deepak <anandhadeepak@gmail.com>
2014-12-08 11:11:18 +0530

    refactored domain

--- lib/poisol/domain.rb
+++ lib/poisol/domain.rb
@@ -2,8 +2,8 @@ class Domain
   attr_reader :file,:full_url
 
   def initialize domain_config_file
-    @file = domain_config
-    base_hash = Parse.yaml_file domain_config
+    @file = domain_config_file
+    base_hash = Parse.yaml_file @file
     domain  = base_hash["domain"]
     port = base_hash["port"]
     @full_url = "#{domain.chomp('\\')}#{ port.present? ? ":#{port}" : "" }"
